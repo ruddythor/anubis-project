c45a413cab44b8c83c4c908c9621b77b0ef95d53
Josh <josh.s.brown@hotmail.com>
2022-05-22 21:13:55 -0400

    gmaps

--- anubisgui.py
+++ anubisgui.py
@@ -1,6 +1,22 @@
 
 import PySimpleGUI as sg
+import googlemaps
+from datetime import datetime
 
+gmaps = googlemaps.Client(key='Add Your Key here')
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
 # sg.Window(title="Hello World", layout=[[]], margins=(100, 50)).read()
 
 
@@ -18,5 +34,6 @@ while True:
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
     print('You entered ', values[0])
+    print(directions_result)
 
 window.close()
